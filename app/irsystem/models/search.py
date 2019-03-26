54fc566732d9d9c2d1fb31da0b0a6498a89508bd
Claire Han <cjh286@cornell.edu>
2019-03-26 16:36:02 -0400

    start code for preprocessing

--- app/irsystem/models/search.py
+++ app/irsystem/models/search.py
@@ -1,1 +1,12 @@
 #this is where you implement your search
+
+def preprocess(file):
+    """
+    preprocessing function
+    """
+    file = open(file, "r")
+
+    pass
+
+def createInvertedIndex():
+    pass
