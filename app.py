73fb2618587fc02c4c652ed651509208fd0a95df
Claire Han <cjh286@cornell.edu>
2019-03-26 13:06:03 -0400

    code to open browser automatically

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from app import app, socketio
+# import webbrowser
 
 if __name__ == "__main__":
   print("Flask app running at http://0.0.0.0:5000")
+  # webbrowser.open_new_tab("http://localhost:5000/")
   socketio.run(app, host="0.0.0.0", port=5000)
