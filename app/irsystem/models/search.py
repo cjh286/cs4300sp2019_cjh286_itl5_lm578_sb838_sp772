4e0ac9ed591ea53ee674b1a057950be5c050c82e
Claire Han <cjh286@cornell.edu>
2019-04-15 11:50:23 -0400

    changed complement ranking for testing

--- app/irsystem/models/search.py
+++ app/irsystem/models/search.py
@@ -256,7 +256,7 @@ def main():
     co_oc = makeCoOccurrence(recipe_dict, len(all_ingredients_list), indexTermDict[1])
 
     query = ['orange juice']
-    rankings = complementRanking(query, co_oc, indexTermDict[0], indexTermDict[1])
+    rankings = complementRanking(query, co_oc, indexTermDict[1], indexTermDict[0])
     print(rankings)
 
 # for testing only
