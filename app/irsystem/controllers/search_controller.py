eb1d3b8417ddc0de0080e8b74f27b985831d82c6
Claire Han <cjh286@cornell.edu>
2019-04-26 17:01:45 -0400

    sessions

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -10,25 +10,22 @@ import copy
 # global variables - requires storage
 project_name = "Drink Up!"
 net_id = "cjh286, itl5, lm578, sb838, sp772"
-cocktail = []
-rankings = None
-output_message = ""
-ingredients = None
-searchBy = "ingredients"
-xable = []
 
 @irsystem.route('/', methods=['GET'])
 def search():
-	# define global variables
-	global cocktail
-	global rankings
-	global output_message
-	global ingredients
-	global searchBy
-	global xable
+	if (session['cocktail'] == None):
+		session['cocktail'] = []
+	if (session['output_message'] == None):
+		session['output_message'] = ""
+	if (session['searchBy'] == None):
+		session['searchBy'] = 'ingredients'
+	if (session['xable'] == None):
+		session['xable'] = []
+	if (session['rankings'] == None):
+		session['rankings'] = []
 
-	if (request.args.get('ingredients') != None):
-		ingredients = request.args.get('ingredients')
+
+	ingredients = request.args.get('ingredients')
 	addToCocktail = request.args.get('add-to-cocktail')
 	addQueryToCocktail = request.args.get('add-query-to-cocktail')
 	clearCocktail = request.args.get('clear-cocktail')
@@ -36,7 +33,7 @@ def search():
 	removeFromQuery = request.args.get('remove-from-query')
 	about = request.args.get('about')
 	if (request.args.get('search-label') != None):
-		searchBy = request.args.get('search-label')
+		session['searchBy'] = request.args.get('search-label')
 	done = request.args.get('done-cocktail')
 
 	# set up - build all necessary datasets
@@ -49,29 +46,34 @@ def search():
 	labeled_dict = do_ml(ml_ingred_list)
 	flavor_dict = create_flavor_dict()
 
-	# user wants to search by
-	if not searchBy:
-		searchBy = "ingredients"
 
 	# user searched ingredients
-	if not ingredients:
-		rankings = []
-		xable = []
-		output_message = ""
+	if (ingredients == ""):
+		session['rankings'] = None
+		session['xable'] = []
+		session['output_message'] = ""
+		# session['ingredients'] = None
 	else:
-		output_message = "Your Search: " + ingredients
-		query = ingredients.split(', ')
-		query = queryReformulation(query, all_ingredients_list)
-		for q in query:
-			if q not in xable:
-				xable.append(q)
-		initial_rank = complementRanking(xable, co_oc, indexTermDict[1], indexTermDict[0])
-		rankings = displayRanking(initial_rank, lower_to_upper_i, labeled_dict, flavor_dict, searchBy)
+		if (ingredients != None):
+			output_message = "Your Search:"
+			query = ingredients.split(', ')
+			query = queryReformulation(query, all_ingredients_list)
+			xable = session.pop('xable', [])
+			for q in query:
+				if q not in xable:
+					xable.append(q)
+			initial_rank = complementRanking(xable, co_oc, indexTermDict[1], indexTermDict[0])
+			session['rankings'] = displayRanking(initial_rank, lower_to_upper_i, labeled_dict, flavor_dict, session['searchBy'])
+			session['xable'] = xable
+
 
 	# user wants to remove an item from their running query
 	if removeFromQuery:
+		xable = session.pop('xable', [])
 		if (removeFromQuery in xable):
 			xable.remove(removeFromQuery)
+			session['xable'] = xable
+
 
 	# user wants to add the item they queried to the cocktail
 	if addQueryToCocktail:
@@ -80,32 +82,29 @@ def search():
 			addSearchedIngredient = lower_to_upper_i[addQueryToCocktail]
 		else:
 			addSearchedIngredient = addQueryToCocktail
-		if (addSearchedIngredient not in cocktail) and (addSearchedIngredient != None):
-			cocktail.append(addSearchedIngredient)
+		if (addSearchedIngredient not in session['cocktail']) and (addSearchedIngredient != None):
+			session['cocktail'].append(addSearchedIngredient)
 
 	# user wants to add item to cocktail
-	if addToCocktail:
-		# addIngredient = getNameFromRanking(addToCocktail)
-		if (addToCocktail not in cocktail) and (addToCocktail != None):
-			cocktail.append(addToCocktail)
+	if (addToCocktail != None) and (addToCocktail not in session['cocktail']):
+		cocktail = session.pop('cocktail', [])
+		cocktail.append(addToCocktail)
+		session['cocktail'] = cocktail
 
 	# user wants to clear the cocktail list
 	if clearCocktail:
-		cocktail = []
+		session['cocktail'] = []
 
 	# user wants to remove an item from the cocktail list
 	if removeFromCocktail:
-		cocktail.remove(removeFromCocktail)
+		session['cocktail'].remove(removeFromCocktail)
 
 	# user is done building recipe
 	if done:
-		ingred_query = queriesForCocktail(cocktail)
+		ingred_query = queriesForCocktail(session['cocktail'])
 		cocktail_list = makeCocktailRanks(ingred_query, makeJaccard, recipe_dict)
-		return render_template('cocktails.html', cocktail=cocktail, cocktail_search = cocktail_list)
+		return render_template('cocktails.html', cocktail=session['cocktail'], cocktail_search=cocktail_list)
 
-	# if xable:
-	# 	initial_rank = complementRanking(xable, co_oc, indexTermDict[1], indexTermDict[0])
-	# 	rankings = displayRanking(initial_rank, lower_to_upper_i, labeled_dict, flavor_dict, searchBy)
 
 	# takes the user to the about page
 	if about:
@@ -114,5 +113,5 @@ def search():
 	# removed searched=ingredients for testing purposes
 	return render_template('search.html', name=project_name, netid=net_id, \
 		complete_ingredients=json.dumps(auto_ingredients_list), \
-			output_message=output_message, data=rankings, cocktail=cocktail, \
-				search_by=searchBy, xable=xable)
+			output_message=session['output_message'], data=session['rankings'], \
+			cocktail=session['cocktail'], search_by=session['searchBy'], xable=session['xable'])
