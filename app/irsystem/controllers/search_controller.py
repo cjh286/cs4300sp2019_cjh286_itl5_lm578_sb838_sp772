d18de2eb5622079742a4b9ecbaaa99dd4308a13f
Claire Han <cjh286@cornell.edu>
2019-04-26 19:43:04 -0400

    fixed lemon juice bug

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -10,9 +10,11 @@ import copy
 # global variables - requires storage
 project_name = "Drink Up!"
 net_id = "cjh286, itl5, lm578, sb838, sp772"
+search_by_options = ['alcohol', 'mixer', 'garnish']
 
 @irsystem.route('/', methods=['GET'])
 def search():
+	global search_by_options
 	if ('cocktail' not in session):
 		session['cocktail'] = []
 	if ('output_message' not in session):
@@ -34,8 +36,10 @@ def search():
 	removeFromCocktail = request.args.get('remove-from-cocktail')
 	removeFromQuery = request.args.get('remove-from-query')
 	about = request.args.get('about')
-	if (request.args.get('search-label') != None):
+	if (request.args.get('search-label') in search_by_options):
 		session['searchBy'] = request.args.get('search-label')
+	else:
+		session['searchBy'] = 'ingredients'
 	done = request.args.get('done-cocktail')
 
 	# set up - build all necessary datasets
@@ -54,9 +58,11 @@ def search():
 		session['rankings'] = []
 		session['xable'] = []
 		session['output_message'] = ""
+		session.modified = True
 		# session['ingredients'] = None
 	else:
 		if (ingredients != None):
+			rankings = session.pop('rankings', [])
 			output_message = "Your Search:"
 			query = ingredients.split(', ')
 			query = queryReformulation(query, all_ingredients_list)
@@ -65,7 +71,9 @@ def search():
 				if q not in xable:
 					xable.append(q)
 			initial_rank = complementRanking(xable, co_oc, indexTermDict[1], indexTermDict[0])
-			session['rankings'] = displayRanking(initial_rank, lower_to_upper_i, labeled_dict, flavor_dict, session['searchBy'])
+			rankings = displayRanking(initial_rank, lower_to_upper_i, labeled_dict, flavor_dict, session['searchBy'])[:50]
+			
+			session['rankings'] = rankings
 			session['xable'] = xable
 
 
@@ -79,13 +87,15 @@ def search():
 
 	# user wants to add the item they queried to the cocktail
 	if addQueryToCocktail:
+		cocktail = session.pop('cocktail', [])
 		# TODO: there are some queries not in lower_to_upper_i b/c apostrophes => fix this
 		if (addQueryToCocktail in lower_to_upper_i):
 			addSearchedIngredient = lower_to_upper_i[addQueryToCocktail]
 		else:
 			addSearchedIngredient = addQueryToCocktail
-		if (addSearchedIngredient not in session['cocktail']) and (addSearchedIngredient != None):
-			session['cocktail'].append(addSearchedIngredient)
+		if (addSearchedIngredient not in cocktail) and (addSearchedIngredient != None):
+			cocktail.append(addSearchedIngredient)
+		session['cocktail'] = cocktail
 
 	# user wants to add item to cocktail
 	if (addToCocktail != None) and (addToCocktail not in session['cocktail']):
@@ -99,7 +109,10 @@ def search():
 
 	# user wants to remove an item from the cocktail list
 	if removeFromCocktail:
-		session['cocktail'].remove(removeFromCocktail)
+		cocktail = session.pop('cocktail', [])
+		if (removeFromCocktail in cocktail):
+			cocktail.remove(removeFromCocktail)
+		session['cocktail'] = cocktail
 
 	# user is done building recipe
 	if done:
@@ -116,4 +129,4 @@ def search():
 	return render_template('search.html', name=project_name, netid=net_id, \
 		complete_ingredients=json.dumps(auto_ingredients_list), \
 			output_message=session['output_message'], data=session['rankings'], \
-			cocktail=session['cocktail'], search_by=session['searchBy'], xable=session['xable'])
+			cocktail=session['cocktail'], search_by=session['searchBy'], xable=session['xable'], searched=ingredients)
